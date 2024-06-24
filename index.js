cb08ada5b45b2a40aee09859ca31e4b7afd860fa
Maia <vincecake008@gmail.com>
2024-06-24 11:59:20 -0300

    feat: moda não finalizada

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-//adiciona a mediana
 console.log("Adicione um número para fazer a média e a mediana: ");
+let maiornumero = 0;
 let numUsuario;
-let numeros = [];
+const numeros = [];
 let media;
 let mediana;
 
 process.stdin.on("data", function (data) {
-numUsuario = Number(data.toString().trim());
+  numUsuario = Number(data.toString().trim());
   numeros.push(numUsuario);
   numUsuario = 0;
   calculaMedia();
   if (numeros.length >= 2) {
-    calculaMediana()
+    calculaMediana();
   }
+  if (numeros.length >= 4) {
+    calculaModa();
+  }
+  console.log(numeros);
 });
 
 function calculaMedia() {
@@ -20,14 +24,26 @@ function calculaMedia() {
   for (let i = 0; i < numeros.length; i++) {
     soma = soma + numeros[i];
     media = soma / numeros.length;
-}
-console.log("média:", media);
+  }
+  console.log("média:", media);
 }
 
 function calculaMediana() {
-    const sortNumeros = numeros.sort((a, b) => a - b) 
-    const  medianaIdenx = Math.floor(sortNumeros.length / 2);
-    mediana = sortNumeros[medianaIdenx]
-    console.log("mediana" , mediana)
-    return mediana;
+  const sortNumeros = numeros.sort((a, b) => a - b);
+  const medianaIdenx = Math.floor(sortNumeros.length / 2);
+  mediana = sortNumeros[medianaIdenx];
+  console.log("mediana: ", mediana);
+  return mediana;
+}
+
+function calculaModa() {
+  let moda;
+  for (let i = 0; i < numeros.length; i++) {
+   // for (let j = i + 1; j < numeros.length; j++) {
+      if (numeros[i].length % 2 == 0) {
+       moda = numeros[i];
+      }
+ //   }
+  }
+  console.log("moda: ", moda);
 }
